d98db1a14e6dc49ac9ab20cf282352882d2feacd
Charuka Kavinda <111011397+Ckavinda98@users.noreply.github.com>
2024-03-29 15:44:50 +0530

    Adding String Practice

---
+++ string/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Hello");
+Console.WriteLine("World");
+
+Console.WriteLine("Hello\nWorld");
+Console.WriteLine("Tab\tTab\tTab");
+Console.WriteLine("C: windows\\lewfile\\file.text");
+Console.WriteLine("this is \"quote\" get as output");
+Console.WriteLine(@"c: windows\newfilw\file.text");
+Console.WriteLine("this is \"quote\"");
+
+var firstName = "Mical, ";
+var lastName = "Aravinda";
+
+var fullName = $"Hello {firstName}";
+
+var greeing = $"Hellowwwww {firstName + lastName}   -- Time is {DateTime.Now}";
+
+Console.WriteLine($"Hello {firstName}");
+Console.WriteLine(greeing);
+
