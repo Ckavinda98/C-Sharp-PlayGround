98298af8ac7d2c7151e10f24b07c6c5a930f990c
Charuka Kavinda <111011397+Ckavinda98@users.noreply.github.com>
2024-03-29 22:03:39 +0530

    Adding For Loop

---
+++ forloop/Program.cs
@@ -0,0 +1,21 @@
+﻿for(int i=1; i<101; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+    {
+        Console.WriteLine($"{i} = FizzeBizze");
+
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"{i} = Bizze");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} = Fizze");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+    
+}
