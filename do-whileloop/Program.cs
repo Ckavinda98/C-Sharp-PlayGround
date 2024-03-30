32eb0ae219ee25c1de76d7fdb70aacb2c01b6127
Charuka Kavinda <111011397+Ckavinda98@users.noreply.github.com>
2024-03-30 14:57:12 +0530

    Adding Do While Loop

---
+++ do-whileloop/Program.cs
@@ -0,0 +1,13 @@
+﻿// int i = 0;
+Random random = new Random();
+int roll = 0;
+int index = 1;
+
+do
+{
+    roll = random.Next(1, 7);
+    roll++;
+    Console.WriteLine($"Get the value of role - {roll} time that run loop - {index}");
+    index++;
+    
+} while(roll !=2);
