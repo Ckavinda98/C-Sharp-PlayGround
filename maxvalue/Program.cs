dceeb3c4b1a119fe817ad5ed085586124efff6cc
Charuka Kavinda <111011397+Ckavinda98@users.noreply.github.com>
2024-03-30 19:17:02 +0530

    Adding Finding Max Value in Array

---
+++ maxvalue/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] values = {20, 5, 79, 59, 89, 12, 30};
+
+// int max  = values[0];
+
+// Using Max Methosd --> First Approach
+// int max  = values.Max();
+
+// Using For Loop Methosd --> Second Approach
+// for(int i = 1; i < values.Length; i++)
+// {
+//     if (values[i] > max)
+//     {
+//         max = values[i];
+//     }
+// }
+
+
+
+// Using Foreach Loop Methosd --> Third Approach
+
+int max = int.MinValue;
+
+foreach(int num in values)
+{
+    if (num > max)
+    {
+        max = num;
+    }
+}
+
+Console.WriteLine($"Max Value of the Array is : {max}");
