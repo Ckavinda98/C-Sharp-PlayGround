26876557c2866b1d0c967ac870ed1b39a93da4f6
Charuka Kavinda <111011397+Ckavinda98@users.noreply.github.com>
2024-03-30 15:14:30 +0530

    Adding While Loop

---
+++ whilelopp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Random random = new Random();
+int roll = random.Next(1, 11);
+
+Console.WriteLine($"First Number is :  {roll}");
+while(roll >= 3)
+{
+    Console.WriteLine($"Get the Roll Number {roll}");
+    roll = random.Next(2, 11);
+}
+
+Console.WriteLine($"Last Number is :  {roll}");
