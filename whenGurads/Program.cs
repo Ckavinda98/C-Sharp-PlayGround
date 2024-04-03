e0735fb8f1015cb1f8eb9a3bc73ce2541f133259
Charuka Kavinda <111011397+Ckavinda98@users.noreply.github.com>
2024-04-03 11:15:13 +0530

    Adding When Gurads

---
+++ whenGurads/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+var people = new List<Person> 
+{
+    new ("mical", "Mical", 19),
+    new ("Kevin", "Kevin", 21),
+    new ("Sadubn", "Sadubn", 12)
+};
+
+
+string checkPerson(Person person)
+{
+    return person switch
+    {
+
+        {Age: var age} when age < 18 => "Minor Person",
+
+        _ => "Adult"
+    };
+}
+
+foreach(var p in people)
+{
+    Console.WriteLine(checkPerson(p));
+}
+
+Console.ReadLine();
+
+record Person(string FirstName, string LastName, int Age);
