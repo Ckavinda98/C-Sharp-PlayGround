f1b6105e542a7c366934a4f7c0f5f75bfc069a93
Charuka Kavinda <111011397+Ckavinda98@users.noreply.github.com>
2024-04-03 10:53:11 +0530

    Adding Logical Type Pattern

---
+++ logicalPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+string CheckGrade(int a) => a switch
+{
+    < 15 => "Your Grade is F",
+     >= 15 and < 35  => "Your Grade is S",
+     >= 35 and < 55  => "Your Grade is C",
+     >= 55 and < 75  => "Your Grade is B",
+    _ => "Your Grade is A"
+};
+
+Console.WriteLine(CheckGrade(69));
