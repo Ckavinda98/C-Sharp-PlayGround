b4a9915f8533e2c39c3564b95703c8180506feea
Charuka Kavinda <111011397+Ckavinda98@users.noreply.github.com>
2024-04-03 09:47:53 +0530

    Adding  Value Pattern

---
+++ patternMatching/Program.cs
@@ -0,0 +1,49 @@
+﻿
+ColorMatching("r");
+ColorMatching("g");
+ColorMatching("b");
+ColorMatching("o");
+ColorMatching("y");
+
+// value pattern
+
+string color = "7";
+
+var result = color switch
+{
+    "r" => "Color Is Red",
+    "g" => "Color Is Green",
+    "b" => "Color Is Blue",
+    "o" => "Color Is Orange",
+    _ => "Unkown Color"
+};
+
+Console.WriteLine(result);
+
+// normal switch case
+
+void ColorMatching(string color)
+{
+    switch(color)
+    {
+        case "r":
+        Console.WriteLine("Color is Red");
+        break;
+
+        case "o":
+        Console.WriteLine("Color is Orange");
+        break;
+
+        case "b":
+        Console.WriteLine("Color is Blue");
+        break;
+
+        case "g":
+        Console.WriteLine("Color is Green");
+        break;
+
+        default:
+        Console.WriteLine("Color is Unvalid");
+        break;
+    }
+}
