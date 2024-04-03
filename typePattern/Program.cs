e0248875426c63804e6c365c62a9aad610577a79
Charuka Kavinda <111011397+Ckavinda98@users.noreply.github.com>
2024-04-03 10:08:24 +0530

    Adding Type Pattern

---
+++ typePattern/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+int age = 13;
+
+string name = "Mical";
+
+List<string> City = new List<string> { "colombo", "gampaha", "Kandy"};
+
+var numbers = new int[] {1, 3, 4, 5, 6, 6};
+
+
+object CheckType(object val) => val switch 
+{
+        int => "Integer Type",
+        string => "String Type",
+        List<string> => "List Type",
+        Array => "Array Type",
+        _ => "Invalid Type"
+};
+
+Console.WriteLine(CheckType(age));
+Console.WriteLine(CheckType(name));
+Console.WriteLine(CheckType(City));
+Console.WriteLine(CheckType(numbers));
