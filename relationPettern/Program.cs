fcb222a1e6ea842fde27736155ed2c4feb5a3566
Charuka Kavinda <111011397+Ckavinda98@users.noreply.github.com>
2024-04-03 10:25:37 +0530

    Adding Relation Pattern

---
+++ relationPettern/Program.cs
@@ -0,0 +1,14 @@
+﻿// Relation Pattern 
+
+
+var numbers = new List<int> {1, -3, -4, 6, 7, 9, 8};
+
+foreach(var num in numbers)
+{
+    var res = num switch
+    {
+        < 0 => $"{num} is a Negative Number",
+        > 0 => $"{num} is a Positive Number"
+    };
+    Console.WriteLine(res);
+};
