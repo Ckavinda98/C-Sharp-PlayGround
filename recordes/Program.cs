49a03411fe698b299945bfbf027d659691d62892
Charuka Kavinda <111011397+Ckavinda98@users.noreply.github.com>
2024-04-05 02:21:19 +0530

    Adding Recodes

---
+++ recordes/Program.cs
@@ -0,0 +1,32 @@
+﻿var mical = new Person("Mical", "Joshep");
+var kamindu = new Person("Mical", "Joshep");
+
+// Console.WriteLine(mical);
+// Console.WriteLine(kamindu);
+
+
+var micalclass = new PersonClass()
+{
+FirstName = "Mical",
+LastName = "Joshep"
+};
+
+
+
+var kamindulclass = new PersonClass()
+{
+    FirstName = "Mical",
+    LastName = "Joshep"
+};
+
+
+Console.WriteLine(kamindulclass == micalclass);
+Console.WriteLine(kamindu == mical);
+
+public class PersonClass
+{
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
+
+} 
+public record Person(string FirstName, string LastName);
